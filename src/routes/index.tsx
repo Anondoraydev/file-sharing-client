e03d9ad6f5bb9ad3451be17442121c86a9599f77
Anondo Ray <anondo554@gmail.com>
2026-01-14 16:08:01 +0600

    axios funtion added and navigation done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import App from "@/App";
 import { LoginForm } from "@/components/modules/Authentication/LoginFrom";
 import { RegisterForm } from "@/components/modules/Authentication/RegisterFrom";
 import About from "@/pages/About";
+import Verify from "@/pages/Verify";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -23,6 +24,9 @@ export const router = createBrowserRouter([
   {
     Component: RegisterForm,
     path: "/register"
-
+  },
+  {
+    Component: Verify,
+    path: "/verify"
   }
 ]);
