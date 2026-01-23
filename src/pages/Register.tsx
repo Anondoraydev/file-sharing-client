e9294d77f6c97ea7d9a16f74e0ad12810008b123
Anondo Ray <anondo554@gmail.com>
2026-01-23 23:18:34 +0600

    bug fix

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -9,6 +9,7 @@ export default function Register() {
                 <a href="#" className="flex items-center gap-2 self-center font-medium">
                     <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
                         <GalleryVerticalEnd className="size-4" />
+                        
                     </div>
                     Acme Inc.
                 </a>
